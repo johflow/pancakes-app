a15fca24bd117bd727088dbb2afd97bf8c67fd8b
johflow <github@willflowers.me>
2026-03-06 10:12:51 -0500

    Added firebase api key

--- firebase.js
+++ firebase.js
@@ -1,17 +1,21 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCsRBj1TWnHSMStAIQsfaBOUVO0ch0KnPc",
+  authDomain: "pancakes-916bd.firebaseapp.com",
+  projectId: "pancakes-916bd",
+  storageBucket: "pancakes-916bd.firebasestorage.app",
+  messagingSenderId: "1003482432218",
+  appId: "1:1003482432218:web:8177f3e1c16029f7de1be3",
+  measurementId: "G-RNP23NY41H"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
