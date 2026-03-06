c62f521a2142e6f7920deba84a20f6a3ffb03f72
johflow <github@willflowers.me>
2026-03-06 10:28:58 -0500

    Made API keys environmental variables for security

--- firebase.js
+++ firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCsRBj1TWnHSMStAIQsfaBOUVO0ch0KnPc",
-  authDomain: "pancakes-916bd.firebaseapp.com",
-  projectId: "pancakes-916bd",
-  storageBucket: "pancakes-916bd.firebasestorage.app",
-  messagingSenderId: "1003482432218",
-  appId: "1:1003482432218:web:8177f3e1c16029f7de1be3",
-  measurementId: "G-RNP23NY41H"
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// This prevents Next.js from initializing Firebase multiple times
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
